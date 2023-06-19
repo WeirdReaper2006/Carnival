ac4f54d74950597a758ecf41c5751c17f57f8343
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-06-19 13:06:47 +0500

    added login

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Carnival
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Food_Products", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
