b8e664eaec9a3bc9938c39f539460d9ec18fdf8f
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-06-20 14:22:00 +0500

    added new view for social media

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carnival.Controllers
+{
+    public class PlatformsController : Controller
+    {
+        // GET: Platforms
+        public ActionResult SocialMedia()
+        {
+            return View();
+        }
+    }
+}
